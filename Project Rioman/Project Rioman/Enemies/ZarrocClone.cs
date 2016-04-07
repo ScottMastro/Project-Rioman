510fad31d099c046a90929dfcc9da043bcb5cfb4
mastros <scott-mastro@hotmail.com>
2016-04-06 22:05:00 -0400

    implemented zarroc clone

--- Project Rioman/Project Rioman/Enemies/ZarrocClone.cs
+++ Project Rioman/Project Rioman/Enemies/ZarrocClone.cs
@@ -11,8 +11,12 @@ namespace Project_Rioman
         private Texture2D attack;
         private Texture2D bullet;
 
+        private const int BULLET_SPEED = 10;
+        private const int BULLET_DAMAGE = 2;
+
         private bool attacking;
         private double attackTime;
+        private double shootTime;
 
         struct ZarrocBullet
         { 
@@ -21,17 +25,9 @@ namespace Project_Rioman
             public int Y;
             public int direction;
 
-            public void Reset()
-            {
-                isAlive = false;
-                X = 0;
-                Y = 0;
-                direction = -1;
-            }
-
         }
 
-        private ZarrocBullet[] bullets = new ZarrocBullet[3];
+        private ZarrocBullet[] bullets = new ZarrocBullet[6];
 
         public ZarrocClone(int type, int r, int c) : base(type, r, c)
         {
@@ -42,6 +38,7 @@ namespace Project_Rioman
             attack = sprites[2];
 
             sprite = stand;
+            drawRect = new Rectangle(0, 0, stand.Width, stand.Height);
 
             attacking = false;
             location.Y -= stand.Height;
@@ -51,33 +48,55 @@ namespace Project_Rioman
 
         protected override void SubUpdate(Rioman player, Bullet[] rioBullets, double deltaTime, Viewport viewport)
         {
-            //implement attacking, shooting
 
             if (isAlive)
             {
-                if (player.Hitbox.Left > GetCollisionRect().Right)
-                    direction = SpriteEffects.FlipHorizontally;
-                else if (player.Hitbox.Right < GetCollisionRect().Left)
-                    direction = SpriteEffects.None;
-
                 if (!attacking)
                 {
                     attackTime += deltaTime;
                     if (attackTime > 2)
-                    {
-                        attacking = true;
-                        attackTime = 0;
-                    }
+                        Attack();
 
                 }
                 else
                 {
                     attackTime += deltaTime;
                     if (attackTime > 0.35)
+                        Stand();
+
+                    shootTime += deltaTime;
+
+                    if (shootTime > 0.15)
                     {
-                        attacking = false;
-                        attackTime = 0;
+                        shootTime = 0;
+                        int index = -1;
+                        for (int i = 0; i <= bullets.Length - 1; i++)
+                            if (!bullets[i].isAlive)
+                                index = i;
+
+
+                        if (index >= 0)
+                        {
+                            bullets[index].isAlive = true;
+                            bullets[index].direction = (FacingLeft() ? -1 : 1);
+                            bullets[index].X = GetCollisionRect().Center.X;
+                            bullets[index].Y = GetCollisionRect().Center.Y - 8;
+
+                        }
                     }
+
+                }
+            }
+
+            for(int i = 0; i<= bullets.Length -1; i++)
+            {
+                if (bullets[i].isAlive)
+                {
+                    bullets[i].X += bullets[i].direction * BULLET_SPEED;
+
+                    if (bullets[i].X < -20 || bullets[i].X > viewport.Width + 20)
+                        bullets[i].isAlive = false;
+
                 }
             }
         }
@@ -91,32 +110,53 @@ namespace Project_Rioman
                     Audio.PlayShieldHit();
 
                 }
+
+            for (int i = 0; i <= bullets.Length - 1; i++)
+                if (bullets[i].isAlive && player.Hitbox.Intersects(new Rectangle(bullets[i].X, bullets[i].Y, bullet.Width, bullet.Height)))
+                {
+                    player.Hit(BULLET_DAMAGE);
+                    bullets[i].isAlive = false;
+                }
         }
 
         protected override void SubDrawEnemy(SpriteBatch spriteBatch)
         {
-            if (attacking)
-            {
-                sprite = attack;
-                drawRect = new Rectangle(0, 0, attack.Width, attack.Height);
-            }
-            else {
-                sprite = stand;
-                drawRect = new Rectangle(0, 0, stand.Width, stand.Height);
-            }
             spriteBatch.Draw(sprite, new Rectangle(location.X, location.Y, drawRect.Width, drawRect.Height),
                 drawRect, Color.White, 0f, new Vector2(), direction, 0);
-
         }
 
         protected override void SubDrawOther(SpriteBatch spriteBatch)
         {
-
             for (int i = 0; i <= bullets.Length - 1; i++)
             {
                 if (bullets[i].isAlive)
                     spriteBatch.Draw(bullet, new Rectangle(bullets[i].X, bullets[i].Y, bullet.Width, bullet.Height), Color.White);
             }
+        }
+
+        private void Stand()
+        {
+            if (FacingLeft())
+                location.X += attack.Width - stand.Width;
+
+            attacking = false;
+            attackTime = 0;
+
+            sprite = stand;
+            drawRect = new Rectangle(0, 0, stand.Width, stand.Height);
+        }
+
+        private void Attack()
+        {
+            if (FacingLeft())
+                location.X -= attack.Width - stand.Width;
+
+            attacking = true;
+            attackTime = 0;
+            shootTime = 0.15;
+
+            sprite = attack;
+            drawRect = new Rectangle(0, 0, attack.Width, attack.Height);
         }
 
         protected override void SubMove(int x, int y)
@@ -130,8 +170,11 @@ namespace Project_Rioman
 
         public override Rectangle GetCollisionRect()
         {
-            if (FacingLeft())
-                return new Rectangle(location.X + stand.Width / 2, location.Y + 10, stand.Width/2, stand.Height - 10);
+            if (FacingLeft() && attacking)
+                return new Rectangle(location.X + stand.Width / 2 + (attack.Width - stand.Width),
+                    location.Y + 10, stand.Width/2, stand.Height - 10);
+            else if(FacingLeft() && !attacking)
+                return new Rectangle(location.X + stand.Width / 2, location.Y + 10, stand.Width / 2, stand.Height - 10);
             else
                 return new Rectangle(location.X, location.Y + 10, stand.Width/2, stand.Height - 10);
 
