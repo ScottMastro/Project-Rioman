7cd6f2ac5c59f9eaf4ac57614cd3b113f9f8afe2
ScottMastro <scott-mastro@hotmail.com>
2016-05-20 00:01:16 -0400

    starting level design of lurkerman stage

--- Project Rioman/Project Rioman/GameState/GameState.cs
+++ Project Rioman/Project Rioman/GameState/GameState.cs
@@ -24,7 +24,7 @@ namespace Project_Rioman
         static GameState()
         {
             prevState = -1;
-            state = Constant.POSTERMAN;
+            state = Constant.LURKERMAN;
             pause = false;
 
         }
